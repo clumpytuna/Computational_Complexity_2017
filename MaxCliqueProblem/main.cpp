b4c3a805ab8d83ae0e526aeba3d252cea444fde1
Clumpy Tuna <anuhinm@mail.ru>
2017-12-17 02:02:27 +0300

    Initial Commit

---
+++ MaxCliqueProblem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MaxCliqueProblem
+//
+//  Created by Clumpy Tuna on 17.12.2017.
+//  Copyright © 2017 Clumpy Tuna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
